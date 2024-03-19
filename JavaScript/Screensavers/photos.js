86bda9726f8839a830f208e2a2190f72ddd23ee7
AmberForrester <DivineIntelligence3.0@gmail.com>
2024-03-19 14:57:34 -0400

    modified

---
+++ JavaScript/Screensavers/photos.js
@@ -0,0 +1,67 @@
+const refreshRate = 5000;
+
+setInterval(function () {
+  window.location.reload();
+}, refreshRate);
+
+function showTime() {
+  const time = new Date();
+
+  const myMonth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+  ];
+
+  var mm = time.getMonth();
+  var dd = time.getDate();
+  var yy = time.getFullYear();
+  var day = time.getDay();
+  var hh = time.getHours();
+  var min = time.getMinutes();
+  var s = time.getSeconds();
+
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let month = myMonth[mm];
+  let myweekday = weekday[day];
+  let greeting = (hh < 12 ? "AM" : "PM");
+
+  let actualTime =
+    myweekday +
+    " " +
+    month +
+    ", " +
+    dd +
+    " " +
+    yy +
+    " " +
+    hh +
+    ":" +
+    min +
+    ":" +
+    s +
+    " " + 
+    greeting;
+
+  document.getElementById("clock").innerText = actualTime;
+}
+
+showTime();
+updateSlideshow();
+
+
